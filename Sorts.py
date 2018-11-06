ae8c47df6b1cedbd2ac4ebacf7ffc1680a202739
Diego Lezama <diegolezama22@gmail.com>
2018-11-05 21:05:01 -0400

    final update

--- Sorts.py
+++ Sorts.py
@@ -2,31 +2,38 @@ import sys
 
 def MergeSort(A:[], p: int, r: int):
 	if p < r:
-		q = (p+r)//2
+		q = (p+(r-1))//2
 		MergeSort(A, p, q)
 		MergeSort(A, q+1, r)
 		Merge(A, p, q, r)
 		
 
 def Merge(A:[], p:int, q: int, r: int):
-	n = q-p+1			# tamaño de A[p...q]
-	m = r-q				# tamaño de A[q+1]
-	L = []
-	R = []
+	n = q-p+1 			# tamaño de A[p...q]
+	m = r-q 			# tamaño de A[q+1]
+	L = [0]*(n)
+	R = [0]*(m)
 	for i in range(0,n):
-		L.append(A[p+i-1])
-	for i in range(0,m):
-		R.append(A[q+i])
+		L[i] = A[p+i]
+	for j in range(0,m):
+		R[j] = A[q+j+1]
 	L.append(sys.maxsize)
 	R.append(sys.maxsize)
-	i, j = 0,0
-	for k in range(p,r):
+	i, j, k = 0, 0, p
+	while i<n and j<m:
 		if L[i]<=R[j]:
 			A[k]=L[i]	# Remover primero de L
-			i=i+1
+			i = i+1
 		else:
 			A[k]=R[j]	# Remover primero de R
-			j=j+1
+			j = j+1
+		k = k+1
+	while i<n:
+		A[k] = L[i]
+		i, k = i+1, k+1
+	while j<m:
+		A[k] = R[j]
+		j, k = j+1, k+1
 
 def InsertionSort(A:[], p:int, r:int):
 	for j in range (p+1, r):
@@ -36,4 +43,4 @@ def InsertionSort(A:[], p:int, r:int):
 			A[i+1]=A[i]
 			i = i-1
 		A[i+1]=key
-	return A
+	return A	
--- TestSorts.py
+++ TestSorts.py
@@ -11,12 +11,10 @@ A = []
 while i!=n:
 	A.append(randint(0,1000))
 	i = i+1
-
 if algoritmo == "MergeSort":
 	Sorts.MergeSort(A, 0, n-1)
 else:
 	Sorts.InsertionSort(A, 0, n)
-
 fin = time.time()
 tiempo = (fin-inicio) * 1000
 print(algoritmo, n, str(tiempo))
